db7d14c2f12d9a5714fe8687b828fe0b7e5bf7b8
Omar Sobh <om.sobh@gmail.com>
2024-06-20 10:39:39 -0700

    added in new face detection models

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            path: false,
+            os: false,
+            encoding: false,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
